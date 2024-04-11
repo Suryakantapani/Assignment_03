f6852c3f148f9ff2f520badb99a7357ad86ded79
CodeScribe <154777571+Suryakantapani@users.noreply.github.com>
2024-04-11 17:54:05 +0530

    Add files via upload

---
+++ Q8.java
@@ -0,0 +1,16 @@
+package Assignment;
+public class Q8 {
+	static int pow(int x, int n) {
+		if(n==0)
+			return 1;
+		if(n==1)
+			return x;
+		
+		return x*pow(x,n-1);
+	}
+public static void main(String[]args) {
+	System.out.println(pow(5,2));
+			
+		
+	}
+}
