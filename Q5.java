b27724560942444bb0c63d5e51cffe38ce2c56e9
CodeScribe <154777571+Suryakantapani@users.noreply.github.com>
2024-04-11 17:22:42 +0530

    Add files via upload

---
+++ Q5.java
@@ -0,0 +1,18 @@
+package Assignment;
+
+public class Q5 {
+	public static <E> void printarray(E[] a) {
+		for(int i=0; i<a.length; i++) {
+		System.out.println(a[i]);	
+		}
+	}
+	public static void main(String[]args) {
+      Integer x[]= {1,2,3,4};
+      printarray(x);
+      String x1[]= {"pink","yeelow","wtet"};
+      printarray(x1);
+      
+      
+	}
+
+}
---
+++ Q6.java
@@ -0,0 +1,18 @@
+package Assignment;
+
+public class Q6 {
+	public static <T> int count(T[] array, T item) {
+		int count=0;
+		for(int i=0; i<array.length; i++) {
+			if(array[i]==item) 
+				count++;
+	}
+	return count;
+	}
+	public static void main(String[]args) {
+		Integer []x= {2,3,4,5,6,4,4,5,6};
+		Integer x1=5;
+		System.out.println(count(x,x1));
+	}
+
+}
