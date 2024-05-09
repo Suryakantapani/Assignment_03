1a5a67171bb99c953e44a5786940a3260c3fa931
CodeScribe <154777571+Suryakantapani@users.noreply.github.com>
2024-05-09 16:31:02 +0530

    Update Q7.java

--- Q7.java
+++ Q7.java
@@ -1,5 +1,5 @@
-package Assignment;
-
+ package Assignment;
+import java.util.Scanner;
 public class Q7 {
 	static int fact(int n) {
 		if(n==1) {
@@ -8,7 +8,10 @@ public class Q7 {
 		return n*fact(n-1);
 	}
 	public static void main(String []args){
-		System.out.println(fact(5));
+		System.out.println("enter the number ");
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		System.out.println(fact(x));
 		
 	}
 
