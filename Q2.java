518075a9b426b59886b4b3a61892e2fdad95396f
CodeScribe <154777571+Suryakantapani@users.noreply.github.com>
2024-05-10 10:27:16 +0530

    Update Q2.java

--- Q2.java
+++ Q2.java
@@ -1,25 +1,19 @@
 package Assignment;
 import java.util.Scanner;
 public class Q2 {
-public static void main(String[]args)
-{
- String a[]= {"pink","yellow","black"};
- try
- {
-	 System.out.println(a[4]);	
- }
-	catch(ArrayIndexOutOfBoundsException e)
- {
-		System.out.println("ingore");
-		try {
-			 Integer.parseInt(a[0]);
+	public static void main(String[]args){
+		String a[]= {"pink","yellow","black"};
+		try{
+			System.out.println(a[4]);	
+	        }
+		catch(ArrayIndexOutOfBoundsException e){
+			System.out.println(e);
+			try {
+				Integer.parseInt(a[0]);
+			}
+			catch(NumberFormatException se) {
+				System.out.println(e);  
+			}
 		}
-		catch(NumberFormatException se) {
-			 System.out.println("ingore 2");  
 	}
- 
- }
-      System.out.println("exception handle");
-}
-
 }
