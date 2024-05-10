0cf332801507ab0b2626504d0a3cbd9ae973f5e1
CodeScribe <154777571+Suryakantapani@users.noreply.github.com>
2024-05-10 10:41:24 +0530

    Update Q5.java

--- Q5.java
+++ Q5.java
@@ -3,16 +3,13 @@ package Assignment;
 public class Q5 {
 	public static <E> void printarray(E[] a) {
 		for(int i=0; i<a.length; i++) {
-		System.out.println(a[i]);	
+			System.out.println(a[i]);	
 		}
 	}
 	public static void main(String[]args) {
-      Integer x[]= {1,2,3,4};
-      printarray(x);
-      String x1[]= {"pink","yeelow","wtet"};
-      printarray(x1);
-      
-      
+		Integer x[]= {1,2,3,4};
+		printarray(x);
+		String x1[]= {"pink","yellow","white"};
+		printarray(x1);
 	}
-
 }
