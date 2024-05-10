52f2c3388c6268f84ba94c80cebb67af13caf7e0
CodeScribe <154777571+Suryakantapani@users.noreply.github.com>
2024-05-10 10:47:39 +0530

    Update HAQ3.java

--- HAQ3.java
+++ HAQ3.java
@@ -1,19 +1,16 @@
 package Assignment;
-
 public class HAQ3 {
-	static void covert(int x) {
+	static void convert(int x) {
 		if(x==0 || x==1){
 			 System.out.print(x);
 			 return;
 		}
-		covert(x/2);
+		convert(x/2);
 		System.out.print(x%2);
-	 
 	}
+	
 	public static void main(String[]args) {
 		int x=456;
-		covert(x);
-		
+		convert(x);	
 	}
-
 }
