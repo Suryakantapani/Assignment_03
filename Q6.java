25e2e0192283a8be74928f7e9b2f0d77fecb0b23
CodeScribe <154777571+Suryakantapani@users.noreply.github.com>
2024-05-10 10:42:23 +0530

    Update Q6.java

--- Q6.java
+++ Q6.java
@@ -6,13 +6,13 @@ public class Q6 {
 		for(int i=0; i<array.length; i++) {
 			if(array[i]==item) 
 				count++;
-	}
+	        }
 	return count;
 	}
+	
 	public static void main(String[]args) {
 		Integer []x= {2,3,4,5,6,4,4,5,6};
 		Integer x1=5;
 		System.out.println(count(x,x1));
 	}
-
 }
